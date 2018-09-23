3a34f0e4956b91bb5c2c498181557ad81c729cc2
atacai <ata.chai10@gmail.com>
2018-09-23 11:57:25 +0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+short_url_data = [
+	{ original_url: 'http://www.google.com', simplified_url: 'http://google.com' },
+	{ original_url: 'http://www.facebook.com', simplified_url: 'http://facebook.com' },
+	{ original_url: 'http://www.twitter.com', simplified_url: 'http://twitter.com' },
+	{ original_url: 'http://www.youtube.com', simplified_url: 'http://youtube.com' },
+]
+
+short_url_data.each do |data|
+  ShortenUrl.create!(data)
+end
